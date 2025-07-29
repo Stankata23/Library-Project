d82d89296635ebc517c6d3c0043255f7f3a91af8
ShadowStudio90 <186976552+Stankata23@users.noreply.github.com>
2025-07-29 22:19:52 +0300

    Добавяне на още карти
    Добавих Crime Fiction, SCI-FI и Romantasy. Всичко бачка

--- src/Pages/Genres.jsx
+++ src/Pages/Genres.jsx
@@ -2,6 +2,9 @@
 import { Link } from 'react-router-dom';
 import Horror from '../assets/Horror.png';
 import Fantasy from '../assets/Fantasy.png'
+import SciFi from '../assets/SCI-FI.png'
+import CriminalFiction from '../assets/CriminalFiction.png'
+import Romantasy from '../assets/Romantasy.png'
 import '../index.css'
 
 
@@ -14,11 +17,15 @@ export default function Genres(){
         <div>
             <h1>Genres</h1>
         </div>
+        <br />
+        <br />
         <div>       
             <Link to='/horror'> <img src={Horror} alt="Horror genre" /> </Link>
             <Link to="/horror"><img src={Fantasy} alt="Fantasy Genre" /> </Link>
-             <Link to="/horror"><img src={Horror} alt="Horror genre" /> </Link>
-            <Link to="/horror"><img src={Horror} alt="Horror genre" /> </Link>
+             <Link to="/horror"><img src={SciFi} alt="Horror genre" /> </Link>
+            <Link to="/horror"><img src={CriminalFiction} alt="Horror genre" /> </Link>
+            <Link to="/horror"><img src={Romantasy} alt="Horror genre" /> </Link>
+        
         </div>
         <br />
         <br />
