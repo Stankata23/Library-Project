91728b24dc2813a6ca3f5e5eb0edd9bf2521acfe
ShadowStudio90 <186976552+Stankata23@users.noreply.github.com>
2025-07-30 02:55:57 +0300

    Oпит за тъмен режим
    Опитах се да направя тъмният режим за сайта, не успях. Трябва да разгледам повече за това.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,83 @@
-import { useState } from 'react'
+// import './App.css'
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import Genres from './Pages/Genres'
+// import MainPage from './Pages/MainPage'
+// import Horror from './Pages/Horror'
+
+// import "@radix-ui/themes/styles.css";
+
+// import ThemeProvider from './RadixUI/ThemeProvider'
+// import ThemeToggleButton from './RadixUI/ThemeToggleBitton'
+
+// function App() {
+//   return (
+//   <>
+    
+//       <ThemeProvider>
+       
+//       <Router>
+//         <Routes>
+//           <Route path="/" element={<MainPage />} />
+//           <Route path="/genres" element={<Genres />} />
+//           <Route path="/horror" element={<Horror />} />
+//         </Routes>
+//         <div class = "fixed-top-right">
+//       <ThemeToggleButton />
+//       </div>
+//       </Router>
+// </ThemeProvider>
+
+//     </>
+//   )
+// }
+
+// export default App
+
+
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Genres from './Pages/Genres'
 import MainPage from './Pages/MainPage'
 import Horror from './Pages/Horror'
 
-function App() {
-  
+import "@radix-ui/themes/styles.css"
+import { Theme } from "@radix-ui/themes"
+
+import ThemeProvider, { ThemeContext } from './RadixUI/ThemeProvider'
+import ThemeToggleButton from './RadixUI/ThemeToggleBitton'
+import { useContext } from 'react'
+
+// This inner component has access to the theme context
+function AppContent() {
+  const { theme } = useContext(ThemeContext)
 
   return (
     <>
-    <Router>
-      <Routes>
-
-        <Route path="/" element = {<MainPage />} />
-         <Route path="/genres" element = {<Genres />} />
-         <Route path="/horror" element = {<Horror />} />
-
-      </Routes>
-    </Router>
+   
+      <Router>
+      
+          <Theme appearance={theme} >
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/genres" element={<Genres />} />
+          <Route path="/horror" element={<Horror />} />
+        </Routes>
+         <div class ="fixed-top-right">
+        <ThemeToggleButton />
+      </div>
+      </Theme>
+      </Router>
+   
+    
     </>
   )
 }
 
-export default App
+// Main app just provides context
+export default function App() {
+  return (
+    <ThemeProvider>
+      <AppContent />
+    </ThemeProvider>
+  )
+}
